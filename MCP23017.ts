2858f7ffbefb138d2f377166bdc6b6ba76a368a8
kubichris <36224601+kubichris@users.noreply.github.com>
2020-09-18 20:42:07 +0200

    Update MCP23017.ts

--- MCP23017.ts
+++ MCP23017.ts
@@ -156,7 +156,7 @@ let intPin = DigitalPin.P16
         writeRegister(adress, REG_MCP.PORT_B_PULLUP, 0x00)   // Outputs 
 
         writeRegister(adress, REG_MCP.PORT_A_INTEN, 0xFF)   // INTerrupt on PORT B
-    //    writeRegister(adress, REG_MCP.PORT_B_IOCON, 35)   // INTerrupt on PORT B
+        writeRegister(adress, REG_MCP.PORT_A_IOCON, 0x42)   // INTerrupt on PORT B
         
 
         writeRegister(adress, REG_MCP.PORT_A_POL, 0xff) //A bemenetek alacsony aktívak!
